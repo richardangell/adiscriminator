06fc5f6e77e1dc8b9f296c9c6cc4000ad701545b
richardangell <richardangell37@gmail.com>
2018-06-02 20:30:22 +0100

    add always passing example test

---
+++ test/test_logistic_regression.py
@@ -0,0 +1,16 @@
+import unittest
+from ADiscriminator import logistic_regression
+
+class TestLogisticRegression(unittest.TestCase):
+
+    def test_1(self):
+        self.assertTrue(True)
+ 
+    def test_2(self):
+        self.assertTrue(True)
+ 
+
+
+if __name__ == '__main__':
+    unittest.main()
+
