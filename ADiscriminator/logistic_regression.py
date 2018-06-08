f0946332f21f763e1152e606be7bbd2779cf46e4
richardangell <richardangell37@gmail.com>
2018-06-08 16:20:27 +0100

    seperate l2 and on regularised fcns

--- ADiscriminator/logistic_regression.py
+++ ADiscriminator/logistic_regression.py
@@ -4,6 +4,11 @@ import scipy.optimize as op
 from sklearn.preprocessing import StandardScaler
 from ADiscriminator import get_adult_dataset
 #import get_adult_dataset
+from sklearn.linear_model import LogisticRegression
+import statsmodels.api as sm
+from sklearn import preprocessing
+
+
 
 def sigmoid(z):
     '''Calculate the sigmoid of all elements of a np array'''
@@ -13,7 +18,7 @@ def sigmoid(z):
     return(g)
 
 
-def cost_function(theta, X, y, regularisation, lambda_, include_first_coef):
+def cost_function(theta, X, y):
     '''Calculate the cost for given theta, X and y'''
     
     m, n = X.shape
@@ -25,35 +30,41 @@ def cost_function(theta, X, y, regularisation, lambda_, include_first_coef):
     x_dot_theta = X.dot(theta)
 
     J = np.sum((-y * np.log(sigmoid(x_dot_theta))) - ((1 - y) * np.log(1 - sigmoid(x_dot_theta)))) / m
-    
-    if regularisation == 'l1':
-
-        if include_first_coef:
-
-            penalty_term = lambda_ * sum(np.abs(theta)) / (2 * m)
-
-        else:
-
-            penalty_term = lambda_ * sum(np.abs(theta[1:])) / (2 * m)
-
-        J = J + penalty_term
-
-    elif regularisation == 'l2':
-
-        if include_first_coef:
-
-            penalty_term = lambda_ * sum(theta ** 2) / (2 * m)
-
-        else:
-
-            penalty_term = lambda_ * sum(theta[1:] ** 2) / (2 * m)
-        
-        J = J + penalty_term
 
     return(J)
 
 
-def gradient(theta, X, y, regularisation, lambda_):
+
+def cost_function_l2(theta, X, y, lambda_, include_first_coef):
+    '''Calculate the cost with l2 penalty for given theta, X and y'''
+    
+    m, n = X.shape
+    
+    theta = theta.reshape((n, 1))
+    
+    y = y.reshape((m,1))
+
+    x_dot_theta = X.dot(theta)
+
+    J = np.sum((-y * np.log(sigmoid(x_dot_theta))) - ((1 - y) * np.log(1 - sigmoid(x_dot_theta)))) / m
+    
+    if include_first_coef:
+
+        penalty_term = lambda_ * sum(theta ** 2) / (2 * m)
+
+    else:
+
+        penalty_term = lambda_ * sum(theta[1:] ** 2) / (2 * m)
+    
+    J = J + penalty_term
+
+    return(J)
+
+
+
+
+
+def gradient(theta, X, y):
     '''Calculate the gradient of the cost function w.r.t. each element of theta'''
     
     m, n = X.shape
@@ -63,25 +74,38 @@ def gradient(theta, X, y, regularisation, lambda_):
     y = y.reshape((m, 1))
 
     grad = (X.T).dot(sigmoid(X.dot(theta)) - y) / m
-    
-    if regularisation == 'l1':
-
-        
-
-    elif regularisation == 'l2':
-
-        penalty_term = (lambda_ / m) * theta
-
-        if not include_first_coef:
-
-            penalty_term[0] = 0
-
-        grad = grad + penalty_term
 
     return(grad.flatten())
 
 
-def logistic_regression(X, y, fit_intercept = True, standardise = True, regularisation = None):
+
+
+
+def gradient_l2(theta, X, y, lambda_, include_first_coef):
+    '''Calculate the gradient of the cost function including l2 penalty term w.r.t. each element of theta'''
+
+    m, n = X.shape
+    
+    theta = theta.reshape((n, 1))
+    
+    y = y.reshape((m, 1))
+
+    grad = (X.T).dot(sigmoid(X.dot(theta)) - y) / m
+
+    penalty_term = (lambda_ / m) * theta
+
+    if not include_first_coef:
+
+        penalty_term[0] = 0
+
+    grad = grad + penalty_term
+
+    return(grad.flatten())
+
+
+
+
+def logistic_regression(X, y, fit_intercept = True, standardise = True, regularisation = None, lambda_ = 0, penalise_intercept = None):
     '''Fit a logistic regression model with optional standardisation and intercept'''
 
     assert isinstance(fit_intercept, bool), 'fit_intercept must be bool'
@@ -90,14 +114,34 @@ def logistic_regression(X, y, fit_intercept = True, standardise = True, regulari
 
     regularisation_valid = ['l1', 'l2', None]
 
-    assert regularisation in regularisation_valid, 
+    assert regularisation in regularisation_valid, \
         'regularisation must be one of %s' % (regularisation_valid)
+
+    if regularisation == 'l1':
+
+        raise ValueError('l1 regularisation not yet supported.')
+
+    penalise_intercept_valid = [True, False, None]
+
+    assert penalise_intercept in penalise_intercept_valid, \
+        'penalise_intercept must be one of %s' % (penalise_intercept_valid)
+
+    # by default if an intercept is included in the model do not include it in the penalisation
+    if penalise_intercept is None:
+
+        penalise_intercept = not(fit_intercept)
 
     model = {}
 
     model['fit_intercept'] = fit_intercept
 
     model['standardise'] = standardise
+
+    model['regularisation'] = regularisation
+
+    model['lambda_'] = lambda_
+
+    model['penalise_intercept'] = penalise_intercept
 
     coef_names = ['x' + str(i) for i in range(1, X.shape[1] + 1)]
 
@@ -121,13 +165,23 @@ def logistic_regression(X, y, fit_intercept = True, standardise = True, regulari
 
     initial_theta = np.zeros(n)
 
-    # using optimiser suggested by stackoverflow user chammu;
-    # https://stackoverflow.com/questions/18801002/fminunc-alternate-in-numpy
-    log_reg = op.minimize(fun = cost_function, 
-                          x0 = initial_theta, 
-                          args = (X, y),
-                          method = 'TNC',
-                          jac = gradient)
+    if regularisation is None:
+
+        # using optimiser suggested by stackoverflow user chammu;
+        # https://stackoverflow.com/questions/18801002/fminunc-alternate-in-numpy
+        log_reg = op.minimize(fun = cost_function, 
+                              x0 = initial_theta, 
+                              args = (X, y),
+                              method = 'TNC',
+                              jac = gradient)
+
+    else:
+
+        log_reg = op.minimize(fun = cost_function_l2, 
+                              x0 = initial_theta, 
+                              args = (X, y, lambda_, penalise_intercept),
+                              method = 'TNC',
+                              jac = gradient_l2)
 
     model['optimisation_results'] = log_reg
 
@@ -181,9 +235,48 @@ if __name__ == '__main__':
 
     adult_X, adult_y = get_adult_dataset.data_to_np(adult)
 
-    log_reg = logistic_regression(X = adult_X, y = adult_y)
+    #log_reg = logistic_regression(X = adult_X, y = adult_y)
+
+    log_reg = logistic_regression(X = adult_X, y = adult_y, regularisation = 'l2', lambda_ = 5, penalise_intercept = True)
+
+    print('own:')
 
     print(log_reg['coefficients'])
 
+    sk_log_reg = LogisticRegression(C = 1, penalty = 'l2', fit_intercept = True)
 
+    sk_log_reg.fit(adult_X, adult_y)
 
+    print('sklearn:')
+
+    print(sk_log_reg.intercept_)
+
+    print(sk_log_reg.coef_ )
+
+    print('sklearn2:')
+
+    adult_X2 = preprocessing.scale(adult_X)
+
+    sk_log_reg2 = LogisticRegression(C = 1/5, penalty = 'l2', fit_intercept = True)
+
+    sk_log_reg2.fit(adult_X2, adult_y)
+
+    print(sk_log_reg2.intercept_)
+
+    print(sk_log_reg2.coef_[0] )
+
+    adult_X = np.hstack([np.ones((adult_X.shape[0], 1)), adult_X])
+
+    glm = sm.GLM(adult_y, adult_X, family = sm.families.Binomial(link = sm.families.links.logit))
+
+    glm_result = glm.fit_regularized(method = 'elastic_net', alpha = 0, L1_wt = 0)
+
+    glm_result2 = glm.fit()
+
+    print('statsmodels l2')
+
+    print(glm_result.params)
+
+    print('statsmodels')
+
+    print(glm_result2.params)
